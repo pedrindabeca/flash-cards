1bb1cdf5108abef93661afafdce39877155afbfc
pedrindabeca <pedrinho.amaral1011@gmail.com>
2025-06-24 09:45:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Progamação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é Phyton?',
+    'o Phython é uma linguagem de progamação'
+)
