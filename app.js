96ff77495af91d877412550303c099567f443833
pedrindabeca <pedrinho.amaral1011@gmail.com>
2025-07-01 09:40:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('continer')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+<div class="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+        <p>${pergunta}</p>
+</div>
+<div class="cartao_conteudo_resposta">
+        <p>${resposta}</p>
+</div>
+</div>
+`
+let respostaEstaVisivel = false;
+function viraCartao(){
+    
+}
+container.appendChild(cartao)
+}
